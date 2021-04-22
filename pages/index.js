f90b56f0c4344b0857e86ed61109a63399c970c8
Symion Edwards <symion.edwards@gmail.com>
2021-04-22 10:08:59 +0100

    Added mechanism to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+// getServerSideProps runs every time we go to '/'. If we run getStaticProps, will make the request at build time --> this will not reflect updates.
+// Could add revalidate to the props.
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
