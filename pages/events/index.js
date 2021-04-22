b31b5dba8b09af53359ece5a4387e3b2b075dcd8
Symion Edwards <symion.edwards@gmail.com>
2021-04-22 11:33:31 +0100

    Completed frontend ahead of strapi implementation

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,26 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No Events To Show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+// getServerSideProps runs every time we go to '/'. If we run getStaticProps, will make the request at build time --> this will not reflect updates.
+// Could add revalidate to the props.
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
+import Link from "next/link";
 
 export default function Home({ events }) {
-  console.log(events);
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events To Show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
